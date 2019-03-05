fbdd5c2e2dbcafe76e222288c9fcfa2cb2356a07
zfang <747320213@qq.com>
2019-03-05 13:40:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 /**
- * create a deep copy of given json, and resolve it after given callback finish it.
+ * 创建一个给定的json的深拷贝，然后遍历它，遍历会从某个叶节点开始，优先遍历其兄弟节点，然后逐级向上遍历父节点，直到遍历至根节点前结束。每当在遍历中发现新节点时，给定的回调函数就会被调用；当遍历结束时，还会触发给定的结束事件的句柄函数。
  *
  * @export {Function}
  * @param {JSON} json
- * @param {Function} callback callback has 4 arguments: [element, parentObject, key, json]
- * @param {Function} finish this will be excuted at the deadline of traversal. and it has 1 argument: [json]
+ * @param {Function} onMeetNode this callback has 5 arguments: {element, parentObject, path, json, propName}
+ * @param {Function} onFinish this callback will be excuted at the deadline of traversal. and it has 1 argument: [json]
  * @returns {JSON}
  */
-export default async function travelJSON (json, callback, finish) {
+export default async function travelJSON ({json, onMeetNode, onFinish}) {
   json = JSON.parse(JSON.stringify(json)) // deep copy json
-  let travel = async function (currentObject, parentObject) {
-    if (currentObject.__proto__.constuctor === Object) {
-      for (const key in currentObject) {
-        const element = currentObject[key];
-        await callback(element, parentObject, key, json)
-        await travel(element, parentObject)
+  let travel = async function (element, parentObject, propName, path = '') {
+    if (parentObject) path = path+'.'+propName;
+    if (element.__proto__.constuctor === Object) {
+      for (const key in element) {
+        let e = element[key];
+        await travel(e, parentObject, key, path+'.'+key)
       }
     }
-    else if (currentObject.__proto__.constuctor === Array) {
-      for (let key = 0; key < currentObject.length; key++) {
-        let element = currentObject[key];
-        await callback(element, parentObject, key, json)
-        await travel(element, parentObject)
+    else if (element.__proto__.constuctor === Array) {
+      for (let key = 0; key < element.length; key++) {
+        let e = element[key];
+        await travel(e, parentObject, key, path+'.'+key)
       }
     }
+    if (element !== json) await onMeetNode({element, parentObject, propName, json, path})
   }
   await travel(json)
-  if (finish) await finish(json)
+  if (onFinish) await onFinish(json)
   return json
 }
